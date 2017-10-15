9f1afc12261634dca281e47a1afee9a3c204abc6
Adewale Adeleye <Adewagold@yahoo.com>
2017-10-15 15:55:47 +0100

    Vue and bulma added to the project

---
+++ resources/assets/js/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+	<nav class="panel column is-offset-2 is-8">
+  <p class="panel-heading">
+    Phone Book
+  </p>
+  <div class="panel-block">
+
+    <button class="button is-link is-outlined is-fullwidth">
+      Add Contact
+    </button>
+    <button class="button is-link is-outlined is-fullwidth">
+      Edit
+    </button>
+    <button class="button is-link is-outlined is-fullwidth">
+    	Delete
+    </button>
+
+    <p class="control has-icons-left">
+      <input class="input is-small" type="text" placeholder="search">
+      <span class="icon is-small is-left">
+        <i class="fa fa-search"></i>
+      </span>
+    </p>
+  </div>
+ 
+  <a class="panel-block is-active">
+    <span class="panel-icon">
+      <i class="fa fa-book"></i>
+    </span>
+    bulma
+  </a>
+  
+  <div class="panel-block">
+    <button class="button is-link is-outlined is-fullwidth">
+      Add Contact
+    </button>
+  </div>
+</nav>
+</template>
---
+++ resources/views/phonebook.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+<div id="app">
+	<div class="container">
+	<Myheader>	</Myheader>
+	<router-view></router-view>
+	<Myfooter> </Myfooter>	
+	</div>
+	
+	<p v-text="page_title"></p>
+</div>
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
